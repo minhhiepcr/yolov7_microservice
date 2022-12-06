bb43ddd3b0f092d1eab4559ea2dc448a166810d1
minhhiepcr <truongminhhiep246@gmail.com>
2022-12-06 20:49:39 +0700

    Update: load model and predict image

--- src/app.py
+++ src/app.py
@@ -1,21 +1,33 @@
+import onnxruntime as ort
 from flask import Flask, jsonify, request
-
+from common import detect
 
 app = Flask(__name__)
+
+MODEL_FILE_PATH = "src/model/yolov7.onnx"
+
+CUDA_MODE = False
+
+IMG_SIZE = 640
+
+providers = ['CUDAExecutionProvider',
+             'CPUExecutionProvider'] if CUDA_MODE else ['CPUExecutionProvider']
+model = ort.InferenceSession(MODEL_FILE_PATH, providers=providers)
 
 
 @app.route('/api/detectImage', methods=['POST'])
 def detectImage():
     if(request.files):
         binary_file = request.files.get("image_file").read()
-    return "Hello"
+        result = detect(model=model, img=binary_file, img_size=IMG_SIZE)
+    return jsonify(result)
 
 
 if __name__ == '__main__':
     settings = {
         "host": '0.0.0.0',
         "port": 9999,
-        "debug": True,
-        "use_reloader": True
+        "debug": False,
+        "use_reloader": False
     }
     app.run(**settings)
